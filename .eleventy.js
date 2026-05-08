26da096be48f47d89e93212c0e60b074df54c60d
Tim Attewell <tim@covolta.com>
2026-05-08 16:13:18 -0700

    scaffold: eleventy 3.x baseline build

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk", "md", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
